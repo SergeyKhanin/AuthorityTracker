220f88e0b3763416ddfb1c856a692befc3a67cf6
Sergey Khanin <khaninsergeyleonidovich@gmail.com>
2023-08-04 17:03:40 +0300

    chore: Update link

--- Assets/Scripts/CommonCommunitiesPages.cs
+++ Assets/Scripts/CommonCommunitiesPages.cs
@@ -1,5 +1,5 @@
 ﻿public class CommonCommunitiesPages
 {
     public const string Ru = "https://vk.com/starrealms/";
-    public const string En = "https://megahaulin.com/communities-and-leagues/";
+    public const string En = "https://megahaulin.com/";
 }
