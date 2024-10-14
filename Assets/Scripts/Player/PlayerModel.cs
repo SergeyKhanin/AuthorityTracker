1b4c8c35a5ffa0104b83252d61b9e8163a25ee41
Sergey Khanin <khaninsergeyleonidovich@gmail.com>
2024-10-14 12:05:45 +0300

    cleanup

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -3,13 +3,18 @@ namespace Player
     public sealed class PlayerModel
     {
         public int Points { get; private set; }
+        public int Counter { get; private set; }
 
-        public void X1Plus() => Points += 1;
+        public void X1Plus() => Counter += 1;
 
-        public void X5Plus() => Points += 5;
+        public void X5Plus() => Counter += 5;
 
-        public void X1Minus() => Points -= 1;
+        public void X1Minus() => Counter -= 1;
 
-        public void X5Minus() => Points -= 5;
+        public void X5Minus() => Counter -= 5;
+
+        public void Apply() => Points += Counter;
+
+        public void Clear() => Counter = 0;
     }
 }
--- Assets/Scripts/Popup/ConfirmPopupPresenter.cs
+++ Assets/Scripts/Popup/ConfirmPopupPresenter.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Events;
-using UnityEngine;
 
 namespace Popup
 {
