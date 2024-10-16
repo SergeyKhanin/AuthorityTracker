98d09b959b414ab7f53578cec26aebce22bdc735
Sergey Khanin <khaninsergeyleonidovich@gmail.com>
2024-10-16 09:12:13 +0300

    add presenter logic

--- Assets/Scripts/Game/GamePresenter.cs
+++ Assets/Scripts/Game/GamePresenter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Events;
+using UnityEngine.UIElements;
 
 namespace Game
 {
@@ -9,8 +11,34 @@ namespace Game
         public GamePresenter(GameView view)
         {
             _view = view;
+
+            Subscribe();
+            SubscribeToEvents();
         }
 
-        public void Dispose() { }
+        private void OnPauseButtonClicked() => GameEventsManager.PauseOpened.Invoke();
+
+        private void Show() => _view.Container.style.visibility = Visibility.Visible;
+
+        private void Hide() => _view.Container.style.visibility = Visibility.Hidden;
+
+        private void SubscribeToEvents()
+        {
+            GameEventsManager.PauseOpened.AddListener(Hide);
+            GameEventsManager.PauseClosed.AddListener(Show);
+        }
+
+        private void Subscribe()
+        {
+            _view.PauseButton.clicked += OnPauseButtonClicked;
+        }
+
+        public void Dispose()
+        {
+            _view.PauseButton.clicked += OnPauseButtonClicked;
+
+            GameEventsManager.PauseOpened.RemoveListener(Hide);
+            GameEventsManager.PauseClosed.RemoveListener(Show);
+        }
     }
 }
--- Assets/Scripts/Game/PausePresenter.cs
+++ Assets/Scripts/Game/PausePresenter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Events;
+using UnityEngine.UIElements;
 
 namespace Game
 {
@@ -9,8 +11,47 @@ namespace Game
         public PausePresenter(PauseView view)
         {
             _view = view;
+
+            Hide();
+            Subscribe();
+            SubscribeToEvents();
         }
 
-        public void Dispose() { }
+        private void OnQuitButtonClicked() => throw new NotImplementedException();
+
+        private void OnMenuButtonClicked() => throw new NotImplementedException();
+
+        private void OnResetButtonClicked() => throw new NotImplementedException();
+
+        private void OnBackButtonClicked() => GameEventsManager.PauseClosed.Invoke();
+
+        private void Hide() => _view.Container.style.visibility = Visibility.Hidden;
+
+        private void Show() => _view.Container.style.visibility = Visibility.Visible;
+
+        private void SubscribeToEvents()
+        {
+            GameEventsManager.PauseOpened.AddListener(Hide);
+            GameEventsManager.PauseClosed.AddListener(Show);
+        }
+
+        private void Subscribe()
+        {
+            _view.BackButton.clicked += OnBackButtonClicked;
+            _view.ResetButton.clicked += OnResetButtonClicked;
+            _view.MenuButton.clicked += OnMenuButtonClicked;
+            _view.QuitButton.clicked += OnQuitButtonClicked;
+        }
+
+        public void Dispose()
+        {
+            _view.BackButton.clicked += OnBackButtonClicked;
+            _view.ResetButton.clicked += OnResetButtonClicked;
+            _view.MenuButton.clicked += OnMenuButtonClicked;
+            _view.QuitButton.clicked += OnQuitButtonClicked;
+
+            GameEventsManager.PauseOpened.RemoveListener(Hide);
+            GameEventsManager.PauseClosed.RemoveListener(Show);
+        }
     }
 }
