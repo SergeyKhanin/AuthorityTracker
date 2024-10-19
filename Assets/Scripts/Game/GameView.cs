d972c5df462c0f4bf39412d5cd98dcd3ffb4f714
Sergey Khanin <khaninsergeyleonidovich@gmail.com>
2024-10-19 10:14:54 +0300

    add buttons

--- Assets/Scripts/Game/GameView.cs
+++ Assets/Scripts/Game/GameView.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using Extensions;
 using UnityEngine.UIElements;
 
 namespace Game
@@ -7,12 +8,15 @@ namespace Game
     {
         public VisualElement Container { get; private set; }
         public Button PauseButton { get; private set; }
+        public Button HistoryButton { get; set; }
 
         public GameView(UIDocument uiDocument)
         {
             var root = uiDocument.rootVisualElement.Q<VisualElement>(CommonNames.GameViewName);
             Container = root;
             PauseButton = root.Q<Button>("pause-button");
+            HistoryButton = root.Q<Button>("history-button");
+            HistoryButton.Hide();
         }
     }
 }
