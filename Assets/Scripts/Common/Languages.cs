1b3b90745ef16096e62334fe1dbee5aaa748ce41
Sergey Khanin <khaninsergeyleonidovich@gmail.com>
2024-10-20 09:59:00 +0300

    refactor model

--- Assets/Scripts/Common/Languages.cs
+++ Assets/Scripts/Common/Languages.cs
@@ -19,4 +19,24 @@
         Marathi = 14,
         Turkish = 15,
     }
+
+    public static class LanguagesIdentifier
+    {
+        public const string En = "en-US";
+        public const string Zh = "zh-CN";
+        public const string Hi = "hi-IN";
+        public const string Es = "es-ES";
+        public const string Fr = "fr-FR";
+        public const string Ar = "ar-AE";
+        public const string Bn = "bn-IN";
+        public const string Pt = "pt-BR";
+        public const string Ru = "ru-RU";
+        public const string Ur = "ur-IN";
+        public const string Id = "id-ID";
+        public const string Ja = "ja-JP";
+        public const string De = "de-DE";
+        public const string Te = "te-IN";
+        public const string Mr = "mr-IN";
+        public const string Tr = "tr-TR";
+    }
 }
--- Assets/Scripts/Menu/SettingsModel.cs
+++ Assets/Scripts/Menu/SettingsModel.cs
@@ -1,5 +1,7 @@
-﻿using Common;
+﻿using System.Collections.Generic;
+using Common;
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
 
 namespace Menu
@@ -9,16 +11,20 @@ namespace Menu
         private const int PlayerAmount = (int)Players.Player2;
         public Languages Languages { get; private set; }
 
+        private Dictionary<Languages, Locale> _locales;
+
         public SettingsModel()
         {
+            InitializeLanguageLocales();
+
             if (PlayerPrefs.HasKey(CommonNames.LanguageName))
             {
-                SetLanguage((Languages)PlayerPrefs.GetInt(CommonNames.LanguageName));
+                Languages = (Languages)PlayerPrefs.GetInt(CommonNames.LanguageName);
+                SetLanguage(Languages);
             }
             else
             {
                 SetLanguage(Languages.English);
-                PlayerPrefs.Save();
             }
         }
 
@@ -26,18 +32,11 @@ namespace Menu
 
         public void SetLanguage(Languages language)
         {
-            Languages = language;
-            var index = (int)language;
-            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[
-                index
-            ];
-            SaveLanguage(index);
-        }
+            LocalizationSettings.SelectedLocale = GetLocal(language);
 
-        private void SaveLanguage(int index)
-        {
-            PlayerPrefs.SetInt(CommonNames.LanguageName, index);
-            PlayerPrefs.Save();
+            Languages = language;
+
+            SaveLanguage();
         }
 
         public void ResetPlayersData()
@@ -50,8 +49,6 @@ namespace Menu
                     PlayerPrefs.DeleteKey(CommonNames.MaxPointsName + CommonNames.PlayerName + i);
                 }
             }
-
-            PlayerPrefs.Save();
         }
 
         public bool HasPlayersData()
@@ -66,5 +63,58 @@ namespace Menu
         }
 
         public void ResetData() => PlayerPrefs.DeleteAll();
+
+        private void InitializeLanguageLocales()
+        {
+            _locales = new Dictionary<Languages, Locale>();
+            var settings = LocalizationSettings.AvailableLocales;
+
+            _locales.Add(Languages.English, settings.GetLocale(LanguagesIdentifier.En));
+            _locales.Add(Languages.Chinese, settings.GetLocale(LanguagesIdentifier.Zh));
+            _locales.Add(Languages.Hindi, settings.GetLocale(LanguagesIdentifier.Hi));
+            _locales.Add(Languages.Spanish, settings.GetLocale(LanguagesIdentifier.Es));
+            _locales.Add(Languages.French, settings.GetLocale(LanguagesIdentifier.Fr));
+            _locales.Add(Languages.Arabic, settings.GetLocale(LanguagesIdentifier.Ar));
+            _locales.Add(Languages.Bangla, settings.GetLocale(LanguagesIdentifier.Bn));
+            _locales.Add(Languages.Portuguese, settings.GetLocale(LanguagesIdentifier.Pt));
+            _locales.Add(Languages.Russian, settings.GetLocale(LanguagesIdentifier.Ru));
+            _locales.Add(Languages.Urdu, settings.GetLocale(LanguagesIdentifier.Ur));
+            _locales.Add(Languages.Indonesian, settings.GetLocale(LanguagesIdentifier.Id));
+            _locales.Add(Languages.Japanese, settings.GetLocale(LanguagesIdentifier.Ja));
+            _locales.Add(Languages.German, settings.GetLocale(LanguagesIdentifier.De));
+            _locales.Add(Languages.Telugu, settings.GetLocale(LanguagesIdentifier.Te));
+            _locales.Add(Languages.Marathi, settings.GetLocale(LanguagesIdentifier.Mr));
+            _locales.Add(Languages.Turkish, settings.GetLocale(LanguagesIdentifier.Tr));
+        }
+
+        private Locale GetLocal(Languages language)
+        {
+            return language switch
+            {
+                Languages.English => _locales[Languages.English],
+                Languages.Chinese => _locales[Languages.Chinese],
+                Languages.Hindi => _locales[Languages.Hindi],
+                Languages.Spanish => _locales[Languages.Spanish],
+                Languages.French => _locales[Languages.French],
+                Languages.Arabic => _locales[Languages.Arabic],
+                Languages.Bangla => _locales[Languages.Bangla],
+                Languages.Portuguese => _locales[Languages.Portuguese],
+                Languages.Russian => _locales[Languages.Russian],
+                Languages.Urdu => _locales[Languages.Urdu],
+                Languages.Indonesian => _locales[Languages.Indonesian],
+                Languages.Japanese => _locales[Languages.Japanese],
+                Languages.German => _locales[Languages.German],
+                Languages.Telugu => _locales[Languages.Telugu],
+                Languages.Marathi => _locales[Languages.Marathi],
+                Languages.Turkish => _locales[Languages.Turkish],
+                _ => _locales[Languages.English]
+            };
+        }
+
+        private void SaveLanguage()
+        {
+            PlayerPrefs.SetInt(CommonNames.LanguageName, (int)Languages);
+            PlayerPrefs.Save();
+        }
     }
 }
