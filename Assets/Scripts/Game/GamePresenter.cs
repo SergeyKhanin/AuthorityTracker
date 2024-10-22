9bf4d42224eb570a586d050a5888956d31f12376
Sergey Khanin <khaninsergeyleonidovich@gmail.com>
2024-10-22 07:45:17 +0300

    add subscribe to buttons

--- Assets/Scripts/Game/GamePresenter.cs
+++ Assets/Scripts/Game/GamePresenter.cs
@@ -14,6 +14,7 @@ namespace Game
 
             Subscribe();
             SubscribeToEvents();
+            ShowHistoryOpenButton();
         }
 
         private void OnPauseButtonClicked() => EventsManager.PauseOpened.Invoke();
@@ -22,23 +23,43 @@ namespace Game
 
         private void Hide() => _view.Container.Hide();
 
+        private void ShowHistoryOpenButton()
+        {
+            _view.HistoryOpenButton.Show();
+            _view.HistoryCloseButton.Hide();
+        }
+
+        private void HideHistoryOpenButton()
+        {
+            _view.HistoryOpenButton.Hide();
+            _view.HistoryCloseButton.Show();
+        }
+
         private void SubscribeToEvents()
         {
             EventsManager.PauseOpened.AddListener(Hide);
             EventsManager.PauseClosed.AddListener(Show);
+            EventsManager.HistoryClosed.AddListener(ShowHistoryOpenButton);
+            EventsManager.HistoryOpened.AddListener(HideHistoryOpenButton);
         }
 
         private void Subscribe()
         {
             _view.PauseButton.clicked += OnPauseButtonClicked;
+            _view.HistoryOpenButton.clicked += OnPauseButtonClicked;
+            _view.HistoryCloseButton.clicked += OnPauseButtonClicked;
         }
 
         public void Dispose()
         {
             _view.PauseButton.clicked += OnPauseButtonClicked;
+            _view.HistoryOpenButton.clicked -= OnPauseButtonClicked;
+            _view.HistoryCloseButton.clicked -= OnPauseButtonClicked;
 
             EventsManager.PauseOpened.RemoveListener(Hide);
             EventsManager.PauseClosed.RemoveListener(Show);
+            EventsManager.HistoryClosed.RemoveListener(ShowHistoryOpenButton);
+            EventsManager.HistoryOpened.RemoveListener(HideHistoryOpenButton);
         }
     }
 }
