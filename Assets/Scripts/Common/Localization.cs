ae44067d6ac4f1c0caab72248d0d437a9bc8db47
Sergey Khanin <khaninsergeyleonidovich@gmail.com>
2024-10-23 20:02:19 +0300

    add new class

---
+++ Assets/Scripts/Common/Localization.cs
@@ -0,0 +1,27 @@
+﻿namespace Common
+{
+    public static class LocalizationTables
+    {
+        public const string TableReference = "AuthorityTracker";
+        public const string TableEntryReference = "AuthorityTracker";
+        public const string PlaceholderHistory = "Placeholder.History";
+    }
+
+    public static class LocalizationKeys
+    {
+        public const string AppName = "App.Name";
+
+        public const string ButtonApply = "Button.Apply";
+        public const string ButtonClear = "Button.Clear";
+        public const string ButtonSettings = "Button.Settings";
+        public const string ButtonBack = "Button.Back";
+        public const string ButtonRestart = "Button.Restart";
+        public const string ButtonStart = "Button.Start";
+        public const string ButtonContinue = "Button.Continue";
+        public const string ButtonQuit = "Button.Quit";
+        public const string ButtonMenu = "Button.Menu";
+        public const string ButtonClearData = "Button.Clear.Data";
+
+        public const string PlaceholderHistory = "Placeholder.History";
+    }
+}
